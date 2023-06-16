759c245eb0ccaf67f0a0169f71d2c438b94be0ee
poliveira <p.moliveira@yahoo.com>
2023-06-16 09:47:32 -0300

    refactoring password genertor

--- 005/password_generator.py
+++ 005/password_generator.py
@@ -1,27 +1,20 @@
 # password based on the given data.
 #
 
-# Generates a list with the alphabet
-# upper and lower case
-letters = []
-n_letters = []
+letters = ""
 for i in range(65, 65+26):
-    letters.append(chr(i))
+    letters += chr(i)
 
-for i in letters:
-    n_letters.append(i.lower())
+for i in range(97, 97+26):
+    letters += chr(i)
 
-for i in n_letters:
-    letters.append(i)
-
-numbers = []
+numbers = ""
 for i in range(0, 10):
-    numbers.append(str(i))
+    numbers += str(i)
 
-symbols = ["@", "#", "&", "*", "-","+", "-", "+", "=", "$", "/", "%", "?", "!"]
+symbols = "!@#$%&*_-+={}[]?/<>" 
 
 
-print(letters, numbers, symbols)
 
 
 
