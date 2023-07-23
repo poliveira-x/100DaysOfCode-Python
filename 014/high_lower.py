7f15e173ed890d9bf3419ffcd64538222c5d0c17
Pedro Oliveira <p.moliveira@yahoo.com>
2023-07-23 05:28:32 +0000

    014 finished

--- 014/high_lower.py
+++ 014/high_lower.py
@@ -1,10 +1,12 @@
 #
-#
+# High lower game.
+# We chose a name. We keep playing or lose depending whether we
+# pick the right option
 #
 
 from game_data import data
 from random import choice
-
+from os import system
 
 # pick an item
 def pick_new(item1):
@@ -15,34 +17,35 @@ def pick_new(item1):
     return item2
 
 
-'''
-        'name': 'Instagram',
-        'follower_count': 346,
-        'description': 'Social media platform',
-        'country': 'United States'
-
-'''
-
 name1 = choice(data)
-name2 = pick_new(name1)
-
-'''
 score = 0
 
+while True:
+    system('clear') # clear works only on unix-like systems
+    print("High Lower Game")
+    print("==== ===== ====\n")
+    name2 = pick_new(name1)
 
-print("Your current score: {score}")
-'''
+    print(f"Compare A: {name1['name']}, {name1['description']}, {name1['country']}.")
+    print('\nVs\n')
+    print(f"Abainst B: {name2['name']}, {name2['description']}, {name2['country']}.")
 
+    followers = input("\n\nWho has more followers? Type A or B: ").lower()
 
-print(f"Compare A: {name1['name']}, {name1['description']}, {name1['country']}.")
+    if followers == 'a':
+        if name1['follower_count'] > name2['follower_count']:
+            score += 1
+            name1 = name2.copy()
+        else:
+            break
+    elif followers == 'b':
+        if name2['follower_count'] > name1['follower_count']:
+            score += 1
+            name1 = name2.copy()
+        else:
+            break
 
-print('\nVs\n')
-
-print(f"Abainst B: {name2['name']}, {name2['description']}, {name2['country']}.")
-
-
-followers = input("\n\nWho has more followers? Type A or B: ").lower()
-
+print(f"\n\nYou lose!\nYour score: {score}")
 
 
 
