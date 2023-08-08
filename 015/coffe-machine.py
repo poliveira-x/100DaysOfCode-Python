a2b3cdeb2e245d0daf842a57d689c83078322757
Pedro Oliveira <p.moliveira@yahoo.com>
2023-08-08 12:47:42 +0000

    day 015 finished

--- 015/coffe-machine.py
+++ 015/coffe-machine.py
@@ -1,7 +1,4 @@
 #
-#
-#
-
 MENU = {
     "espresso": {
         "ingredients": {
@@ -28,11 +25,15 @@ MENU = {
     }
 }
 
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
+
+
+profit = 0.0
 
 
 #
@@ -41,140 +42,81 @@ resources = {
 
 # Report
 def report():
-    print('\n\n')
-    print('='*50)
-    for i in MENU:
-        print(f"{i}\n{MENU[i]['ingredients']}\n")
-    print('='*50,'\n')
-
-
-
+    print("="*15)
+    for i in resources:
+        print(f"{i}: {resources[i]}")
+    print(f"profit ${profit:,.2f}")
+    print("="*15)
+    
 
 
 # Check suficient ressources
-'''
-4. Check resources sufficient?
-a. When the user chooses a drink, the program should check if there are enough
-resources to make that drink.
-b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
-not continue to make the drink but print: “Sorry there is not enough water.”
-c. The same should happen if another resource is depleted, e.g. milk or coffee.
-'''
-
 def check_sufficient(option):
     for i in MENU[option]['ingredients']:
-        if MENU[option]['ingredients'][i] >= resources[i]:
-            return True
-        else:
-            return i 
+        if resources[i] < MENU[option]['ingredients'][i]:
+            return i
+    return True
 
 
-'''
-1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
-a. Check the user’s input to decide what to do next.
-b. The prompt should show every time action has completed, e.g. once the drink is
-dispensed. The prompt should show again to serve the next customer.
-2. Turn off the Coffee Machine by entering “off” to the prompt.
-a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off
-the machine. Your code should end execution when this happens.
-'''
 
-option = input("What would you like? (espresso/latte/cappuccino): ").lower()
+# Make changes on ingredients and profit
+def inventory(option):
+    for i in MENU[option]['ingredients']:
+        resources[i] -= MENU[option]['ingredients'][i]
 
-resource = check_sufficient(option)
-if resource != True:
-    print(f"Sorry there is not enough {resource}.")
-else:
-    if option == 'espresso':
-        check_sufficient(option)
-        #pass
-        
-    elif option == 'latte':
-        check_sufficient(option)
-        pass
-    elif option == 'cappuccino':
-        pass
-    elif option == 'off':
+    return MENU[option]['cost']
+    
+
+
+# Process Coins
+def process_coins(option):
+    cost =  MENU[option]['cost']
+    print("\nPlease insert coins.")
+    quarters = int(input("How many quarter? "))*0.25
+    dimes = int(input("How many dimes? "))*0.1
+    nickels = int(input("Hop many nickels? "))*0.05
+    pennies = int(input("How many pennies? "))*0.01
+
+    total = quarters+dimes+nickels+pennies
+    
+    if total < cost:
+        print("\nSorry, that's not enough money. Money refunded.\n")
+        return 0
+    elif total > cost:
+        print(f"\nHere is ${total-cost:,.2f} in change.\n")
+    
+    print(f"\nHere is your {option}. Enjoy!\n")
+   
+    return inventory(option)
+
+
+
+#
+# main
+#
+while True:
+    option = input("What would you like? (espresso/latte/cappuccino): ").lower()
+
+    if option == 'off':
         print("\nTurning off...\n")
-        #break
-        pass
+        break
     elif option == 'report':
         report()
+        continue
 
+    if option in ['espresso', 'latte', 'cappuccino']:
+        sufficient = check_sufficient(option)
+        if sufficient != True:
+             print(f"\nThere's not enough {sufficient}.\n")
+             continue
 
-'''
-4. Check resources sufficient?
-a. When the user chooses a drink, the program should check if there are enough
-resources to make that drink.
-b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
-not continue to make the drink but print: “Sorry there is not enough water.”
-c. The same should happen if another resource is depleted, e.g. milk or coffee.
-'''
-
-
-
-
-
-
-'''
-5. Process coins.
-a. If there are sufficient resources to make the drink selected, then the program should
-prompt the user to insert coins.
-b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
-pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
-'''
-
-
-
-
-
-
-'''
-6. Check transaction successful?
-a. Check that the user has inserted enough money to purchase the drink they selected.
-E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
-program should say “Sorry that's not enough money. Money refunded.”.
-b. But if the user has inserted enough money, then the cost of the drink gets added to the
-machine as the profit and this will be reflected the next time “report” is triggered. E.g.
-Water: 100ml
-Milk: 50ml
-Coffee: 76g
-Money: $2.5
-c. If the user has inserted too much money, the machine should offer change.
-E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal
-places.
-'''
-
-
-
-
-
-
-'''
-7. Make Coffee.
-a. If the transaction is successful and there are enough resources to make the drink the
-user selected, then the ingredients to make the drink should be deducted from the
-coffee machine resources.
-E.g. report before purchasing latte:
-Water: 300ml
-Milk: 200ml
-Coffee: 100g
-Money: $0
-Report after purchasing latte:
-Water: 100ml
-Milk: 50ml
-Coffee: 76g
-Money: $2.5
-b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If
-latte was their choice of drink.
-'''
-
-
-
-
-
-
-
+    if option == 'espresso' and sufficient:
+        profit += process_coins(option)
+    elif option == 'latte' and sufficient:
+        profit += process_coins(option)
+    elif option == 'cappuccino' and sufficient:
+        profit += process_coins(option)
+    else:
+        print("\nInvalid option, try again.\n")
 
 
